c583f544de2604dd22842f7e46275ba0d7ee871f
chanwoo <humains29@gmail.com>
2023-05-08 13:37:56 +0900

    인프라: grunt deploy-local 보완

--- grunt/deploy-local.js
+++ grunt/deploy-local.js
@@ -21,10 +21,11 @@ module.exports = async function deployLocal() {
 async function setBuildInfo(newVersionNumber, newBuildNumber) {
   const { execa } = await import("execa");
   const configFilePath = "./Projects/App/xcconfigs/SeeYouAgain.shared.xcconfig";
+  const xcodeProjPath = "./Projects/App/App.xcodeproj"
 
   _setXCConfigValue("MARKETING_VERSION", newVersionNumber, configFilePath);
 
-  await execa("bundle", ["exec", "fastlane", "run", "increment_build_number", `build_number:${newBuildNumber}`], {
+  await execa("bundle", ["exec", "fastlane", "run", "increment_build_number", `build_number:${newBuildNumber}`, `xcodeproj:${xcodeProjPath}`], {
     stdio: "inherit",
   });
 }
@@ -32,6 +33,7 @@ async function setBuildInfo(newVersionNumber, newBuildNumber) {
 function createReleaseCommit(newVersionNumber, newBuildNumber) {
   const configFilePath = "./Projects/App/xcconfigs/SeeYouAgain.shared.xcconfig";
   const commitMessage = `:bookmark: v${newVersionNumber}-${newBuildNumber}`;
+  const infoPlistFilePath = "./Projects/App/Info.plist"
 
   if (shell.exec(`git add ${infoPlistFilePath} ${configFilePath} && git commit -m "${commitMessage}" && git push`).code > 0) {
     shell.echo("❌ 'createReleaseCommit' failed");
@@ -98,7 +100,7 @@ async function checkoutBranch(newVersionNumber, newBuildNumber) {
   shell.echo("✅ 'git fetch' 완료\n");
 
   shell.echo("🌀 'git checkout main' 실행중");
-  await execa("git", ["checkout", "main"]);
+  await execa("git", ["checkout", "develop"]);
   shell.echo("✅ 'git checkout main' 완료\n");
 
   await execa("git", ["pull"]);
