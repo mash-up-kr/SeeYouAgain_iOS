91fb6ee38a4b94b1ac876926aa1171d3083ab011
chanwoo <humains29@gmail.com>
2023-06-03 18:28:44 +0900

    기능: Bubble 뷰 구현

--- Projects/DesignSystem/Sources/Views/Bubble/BubbleView.swift
+++ Projects/DesignSystem/Sources/Views/Bubble/BubbleView.swift
@@ -32,20 +32,30 @@ public struct BubbleView: View {
       label: {
         ZStack {
           Circle()
-            .fill(bubbleColor.backgroundColor)
-            .blur(radius: 18)
+            .fill(
+              LinearGradient(
+                gradient: Gradient(
+                  colors: [
+                    bubbleColor.backgroundFirstColor,
+                    bubbleColor.backgroundSecondColor
+                  ]
+                ),
+                startPoint: .top,
+                endPoint: .bottom
+              )
+            )
             .frame(width: bubbleSize.circleSize, height: bubbleSize.circleSize)
-            .shadow(
-              color: Color(red: 176/255, green: 128/255, blue: 230/255, opacity: 0.3),
+            .optionalShadow(
+              color: bubbleColor.dropShadowColor,
               radius: 24,
               x: 0,
               y: 24
             )
-            .shadow(
-              color: Color(white: 1, opacity: 0.16),
-              radius: 4,
-              x: 2,
-              y: 2
+            .modifier(
+              InnerShadow(
+                color: bubbleColor.innerShadowColor,
+                radius: 4,
+                offset: CGSize(width: 2, height: 2))
             )
           
           Text(keyword)
@@ -103,63 +113,111 @@ public enum BubbleSize: CGFloat {
 
 // MARK: - 버블 배경 및 키워드 폰트 컬러
 public enum BubbleColor {
-  case yellow
-  case green
-  case purple
-  case blue
   case orange
-  case lightGreen
+  case green
+  case violet
+  case blue
+  case red
+  case lime
   case white
   
-  /// 버블 배경 컬러
-  var backgroundColor: Color {
+  /// Inner Shadow 컬러
+  var innerShadowColor: Color {
     switch self {
-    case .yellow:
-      return .yellow
-      
-    case .green:
-      return .green
-      
-    case .purple:
-      return .purple
-      
-    case .blue:
-      return .blue
-      
     case .orange:
-      return .orange
-      
-    case .lightGreen:
-      return .indigo
-      
+      return Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255, opacity: 0.24)
+    case .green:
+      return Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255, opacity: 0.24)
+    case .violet:
+      return Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255, opacity: 0.24)
+    case .blue:
+      return Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255, opacity: 0.24)
+    case .red:
+      return Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255, opacity: 0.24)
+    case .lime:
+      return Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255, opacity: 0.24)
     case .white:
-      return .white
+      return Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255, opacity: 0.24)
+    }
+  }
+  
+  /// Drop Shadow 컬러
+  var dropShadowColor: Color? {
+    switch self {
+    case .orange:
+      return Color(red: 244/255, green: 179/255, blue: 86/255, opacity: 0.3)
+    case .green:
+      return Color(red: 98/255, green: 201/255, blue: 140/255, opacity: 0.3)
+    case .violet:
+      return Color(red: 176/255, green: 128/255, blue: 230/255, opacity: 0.3)
+    case .blue:
+      return Color(red: 114/255, green: 196/255, blue: 244/255, opacity: 0.3)
+    case .red:
+      return Color(red: 244/255, green: 154/255, blue: 106/255, opacity: 0.3)
+    case .lime:
+      return Color(red: 176/255, green: 205/255, blue: 92/255, opacity: 0.3)
+    case .white:
+      return nil
+    }
+  }
+  
+  /// 버블 배경 첫번째 컬러
+  var backgroundFirstColor: Color {
+    switch self {
+    case .orange:
+      return Color(red: 253 / 255, green: 176 / 255, blue: 26 / 255, opacity: 0.8)
+    case .green:
+      return Color(red: 60 / 255, green: 202 / 255, blue: 116 / 255, opacity: 0.7)
+    case .violet:
+      return Color(red: 168 / 255, green: 105 / 255, blue: 234 / 255, opacity: 0.7)
+    case .blue:
+      return Color(red: 69 / 255, green: 178 / 255, blue: 255 / 255, opacity: 0.7)
+    case .red:
+      return Color(red: 252 / 255, green: 110 / 255, blue: 58 / 255, opacity: 0.7)
+    case .lime:
+      return Color(red: 177 / 255, green: 208 / 255, blue: 38 / 255, opacity: 0.7)
+    case .white:
+      return Color(red: 194 / 255, green: 194 / 255, blue: 194 / 255, opacity: 0.4)
+    }
+  }
+  
+  /// 버블 배경 두번째 컬러
+  var backgroundSecondColor: Color {
+    switch self {
+    case .orange:
+      return Color(red: 255 / 255, green: 161 / 255, blue: 30 / 255, opacity: 0.8)
+    case .green:
+      return Color(red: 46 / 255, green: 192 / 255, blue: 105 / 255, opacity: 0.7)
+    case .violet:
+      return Color(red: 158 / 255, green: 89 / 255, blue: 235 / 255, opacity: 0.7)
+    case .blue:
+      return Color(red: 69 / 255, green: 187 / 255, blue: 255 / 255, opacity: 0.7)
+    case .red:
+      return Color(red: 255 / 255, green: 129 / 255, blue: 57 / 255, opacity: 0.7)
+    case .lime:
+      return Color(red: 158 / 255, green: 199 / 255, blue: 38 / 255, opacity: 0.7)
+    case .white:
+      return Color(red: 194 / 255, green: 194 / 255, blue: 194 / 255, opacity: 0.4)
     }
   }
   
   /// 버블 키워드 폰트 컬러
   var fontColor: Color {
     switch self {
-    case .yellow:
-      return .white
-      
-    case .green:
-      return .white
-      
-    case .purple:
-      return .white
-      
-    case .blue:
-      return .white
-      
     case .orange:
       return .white
-      
-    case .lightGreen:
+    case .green:
       return .white
-      
+    case .violet:
+      return .white
+    case .blue:
+      return .white
+    case .red:
+      return .white
+    case .lime:
+      return .white
     case .white:
-      return .black
+      return Color(red: 69 / 255, green: 72 / 255, blue: 73 / 255)
     }
   }
 }
---
+++ Projects/DesignSystem/Sources/Views/Bubble/InnerShadow.swift
@@ -0,0 +1,34 @@
+//
+//  InnerShadow.swift
+//  DesignSystem
+//
+//  Created by GREEN on 2023/06/03.
+//  Copyright © 2023 mashup.seeYouAgain. All rights reserved.
+//
+
+import SwiftUI
+
+public struct InnerShadow: ViewModifier {
+  public var color: Color
+  public var radius: CGFloat
+  public var offset: CGSize
+  
+  public func body(content: Content) -> some View {
+    content
+      .overlay(
+        Circle()
+          .stroke(color, lineWidth: radius)
+          .offset(offset)
+          .mask(
+            Circle()
+              .fill(
+                LinearGradient(
+                  gradient: Gradient(colors: [Color.white, Color.clear]),
+                  startPoint: .top,
+                  endPoint: .bottom)
+              )
+          )
+          .blur(radius: radius)
+      )
+  }
+}
