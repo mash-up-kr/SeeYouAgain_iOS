d5d0fb3abc6010a18773550ddc61b900fac9e76a
Chanwoo Cho <72292617+GREENOVER@users.noreply.github.com>
2023-06-03 15:28:44 +0900

    기능: 폰트 리소스 추가 및 수정 (#17)
    * 기능: Pretendard 폰트 리소스 재설정
    
    * 수정: 네이밍 변경
    
    * 스타일: 린트 룰 적용

--- Projects/Core/Services/_Foundation/NetworkEventLogger.swift
+++ Projects/Core/Services/_Foundation/NetworkEventLogger.swift
@@ -78,8 +78,8 @@ final class NetworkEventLogger: EventMonitor {
 fileprivate extension Data {
   var toPrettyPrintedString: String? {
     guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
-          let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
-          let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
+      let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+      let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
     else {
       return nil
     }
