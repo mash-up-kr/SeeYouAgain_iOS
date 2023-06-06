d29cb7980d74f4c8c96d6287b7a6a7bc43a6c156
Chanwoo Cho <72292617+GREENOVER@users.noreply.github.com>
2023-06-06 10:00:36 +0900

    기능: DS 컬러 리소스 생성 및 DS 바텀 버튼 추가 (#27)
    * 기능: DS 컬러 추가
    
    * 기능: BottomButton 컴포넌트 구현

---
+++ Projects/DesignSystem/Sources/Views/Button/BottomButton.swift
@@ -0,0 +1,54 @@
+//
+//  BottomButton.swift
+//  DesignSystem
+//
+//  Created by GREEN on 2023/06/05.
+//  Copyright © 2023 mashup.seeYouAgain. All rights reserved.
+//
+
+import SwiftUI
+
+public struct BottomButton: View {
+  let title: String
+  var disabled: Bool
+  var action: () -> Void
+  
+  public init(
+    title: String,
+    disabled: Bool = false,
+    action: @escaping () -> Void = {}
+  ) {
+    self.title = title
+    self.disabled = disabled
+    self.action = action
+  }
+  
+  public var body: some View {
+    HStack {
+      Spacer()
+        .frame(width: 20)
+      
+      Button(
+        action: action,
+        label: {
+          HStack {
+            Spacer()
+            
+            Text(title)
+              .font(.r16)
+              .foregroundColor(DesignSystem.Colors.white)
+            
+            Spacer()
+          }
+          .frame(height: 52)
+          .background(DesignSystem.Colors.blue200)
+          .cornerRadius(12)
+        }
+      )
+      .disabled(disabled)
+      
+      Spacer()
+        .frame(width: 20)
+    }
+  }
+}
