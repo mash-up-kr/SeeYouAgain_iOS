b7c72bffa1b1912f1a448d209072666c16770a5a
younggyun <kyoungyun98@naver.com>
2023-06-09 16:47:25 +0900

    수정: 이니셜라이저 수정

--- Projects/Features/Coordinator/AppCoordinator/AppCoordinatorCore.swift
+++ Projects/Features/Coordinator/AppCoordinator/AppCoordinatorCore.swift
@@ -74,7 +74,7 @@ public let appCoordinatorReducer: Reducer<
       case .routeAction(_, action: .setCategory(._sendSelectedCategory)):
         state.routes = [
           .root(
-            .tabBar(.init(hotKeyword: .init(), main: .init(), myPage: .init())),
+            .tabBar(.init(hotKeyword: .init(), main: .init(), myPage: .init(), categoryBottomSheet: .init())),
             embedInNavigationView: true
           )
         ]
