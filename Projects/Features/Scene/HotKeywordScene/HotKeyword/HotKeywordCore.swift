736b992b6634349dbd3c139084f4aa810b1ffb81
Lina.L <lina@weversecompany.com>
2023-06-18 23:02:09 +0900

    기능: HotKeyword 화면 기본 구현

--- Projects/Features/Scene/HotKeywordScene/HotKeyword/HotKeywordCore.swift
+++ Projects/Features/Scene/HotKeywordScene/HotKeyword/HotKeywordCore.swift
@@ -9,29 +9,86 @@
 import Combine
 import ComposableArchitecture
 import Foundation
+import Models
+import Services
+import SwiftUI // 요상하네 이거 있는거
 
 public struct HotKeywordState: Equatable {
+  // 인덱스 숫자가 작을수록 큰 원
+  public var hotKeywordList: [String] = []
+  public var subTitleText: String = ""
+  public var hotKeywordPointList = HotKeywordPointList(
+    hotkeywordList: [],
+    hotKeywordPattern: HotKeywordPatternSpace.generatePattern()
+  )
+    
   public init() { }
 }
 
 public enum HotKeywordAction: Equatable {
   // MARK: - User Action
+  case viewDidLoad
+  case _pullToRefresh
   
   // MARK: - Inner Business Action
+  case _fetchData
   
   // MARK: - Inner SetState Action
+  case _showAnimation
   
   // MARK: - Child Action
+  case _reloadData(HotKeywordDTO)
 }
 
 public struct HotKeywordEnvironment {
+  let hotKeywordService: HotKeywordService = .live
+
   public init() {}
 }
 
 public let hotKeywordReducer = Reducer.combine([
   Reducer<HotKeywordState, HotKeywordAction, HotKeywordEnvironment> { state, action, env in
     switch action {
-    default: return .none
+    case .viewDidLoad:
+      return Effect(value: ._fetchData)
+      
+    case ._pullToRefresh:
+      return Effect(value: ._fetchData)
+      
+    case ._fetchData: // _fetchData랑 _reloadData 이름을 어케바꾸지
+      return env.hotKeywordService.fetchHotKeyword()
+        .catchToEffect() // 이게몰까 여기 메서드들 공부 필요
+        .flatMapLatest { result -> Effect<HotKeywordAction, Never> in
+          switch result {
+          case let .success(hotKeywordDTO):
+            return Effect(value: ._reloadData(hotKeywordDTO))
+            
+            // TODO: 에러처리 확인
+          case let .failure(error):
+            if let error = error.toProviderError() {
+              return .none
+            } else {
+              return .none
+            }
+          }
+        }
+        .eraseToEffect()
+      
+    case let ._reloadData(hotKeywordDTO): // _fetchData랑 _reloadData 이름을 어케바꾸지
+      state.subTitleText = hotKeywordDTO.createdAt
+      state.hotKeywordList = hotKeywordDTO.ranking
+      
+      let currentPattern = state.hotKeywordPointList.pattern
+      state.hotKeywordPointList = HotKeywordPointList(
+        hotkeywordList: state.hotKeywordList,
+        hotKeywordPattern: HotKeywordPatternSpace.generatePattern(without: currentPattern)
+      )
+      return .none
+      
+      // 탭 눌렀을때 호출
+    case ._showAnimation:
+      // TODO: 패턴은 그대로 뷰는 다시 그리기
+      return .none
     }
   }
 ])
--- Projects/Features/Scene/HotKeywordScene/HotKeyword/HotKeywordView.swift
+++ Projects/Features/Scene/HotKeywordScene/HotKeyword/HotKeywordView.swift
@@ -7,8 +7,11 @@
 //
 
 import ComposableArchitecture
+import DesignSystem
 import SwiftUI
 
+// TODO: 1.API 연동 작업, 2.써클 tap, 3. 써클 블러, 4. 디자인 검수(써클 패턴 묘하게 안맞는거 확인 해야할듯, 쪼꼬미 원 추가)
+// 5. 패턴 추가, 6. 진입했을때 애니메이션 시작하기, 7. 리프레시 컨트롤 및, 시작시 데이터 받아오기(패턴 랜덤화), 8. 클릭시 액션
 public struct HotKeywordView: View {
   private let store: Store<HotKeywordState, HotKeywordAction>
   
@@ -16,17 +19,158 @@ public struct HotKeywordView: View {
     self.store = store
   }
   
+  @Namespace var leadingID
+
+  // TODO: 여기 값들 전부 스토어로 넘길 수 있을까?
+  @State private var offset: CGFloat = UIScreen.main.bounds.width
+  @State private var isRefresh: Bool = false // 사용안함
+  
+  private let keyWindow = UIApplication.shared.connectedScenes
+    .compactMap { $0 as? UIWindowScene }
+    .flatMap { $0.windows }
+    .first { $0.isKeyWindow }
+  private var screenWidth: CGFloat {
+    UIScreen.main.bounds.width
+  }
+  private var bubbleViewIdealHeight: CGFloat {
+    let topSafearea = keyWindow?.safeAreaInsets.top ?? 0
+    let bottomSafearea = keyWindow?.safeAreaInsets.bottom ?? 0
+    let titleViewHeight: CGFloat = 98
+    let tabberHeight: CGFloat = 82
+    return UIScreen.main.bounds.height - (topSafearea + bottomSafearea + titleViewHeight + tabberHeight)
+  }
+  
   public var body: some View {
     WithViewStore(store) { viewStore in
-      VStack {
-        HStack {
-          Text("실시간 핫 키워드 화면")
-          Spacer()
-        }
+      ScrollView(.vertical, showsIndicators: false) {
+        titleView
+        
+        bubbleChartView
+        
+        // 하단 탭바 높이만큼 여백
         Spacer()
+          .frame(height: 82)
       }
-      .background(Color.red)
+      .background(DesignSystem.Colors.blue50)
+      .refreshable {
+        offset = screenWidth - screenWidth / 4
+        isRefresh = true
+
+        viewStore.send(._pullToRefresh)
+      }
     }
     .navigationBarHidden(true)
+  }
+  
+  private var titleView: some View {
+    WithViewStore(store) { viewStore in
+      VStack(spacing: 4) {
+        HStack(spacing: 10) {
+          // TODO: title 이미지 변경, 불꽃 이미지 교체
+          DesignSystem.Icons.hotkeywordTitle
+          DesignSystem.Icons.hotkeywordFire
+          Spacer()
+        }
+        .frame(height: 30)
+        
+        HStack {
+          Text(viewStore.subTitleText)
+            .font(.r13)
+            .foregroundColor(DesignSystem.Colors.grey70)
+            .frame(height: 16)
+          Spacer()
+        }
+      }
+      .padding(.vertical, 24)
+      .padding(.horizontal, 24)
+    }
+  }
+  
+  // MARK: - Buuble Chart
+  private var bubbleChartView: some View {
+    WithViewStore(store) { viewStore in
+      ScrollViewReader { proxy in
+        GeometryReader { geometry in
+          ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+              scrollObservableView
+                .id(leadingID)
+              
+              BubbleChartView(
+                hotKeyword: viewStore.hotKeywordPointList,
+                offset: $offset
+              )
+              .frame(
+                width: geometry.size.height * 1.5,
+                height: geometry.size.height
+              )
+            }
+            .onPreferenceChange(ScrollOffsetKey.self) { value in
+              offset = value
+            }
+          }
+        }
+        .frame(
+          idealWidth: screenWidth,
+          idealHeight: bubbleViewIdealHeight
+        )
+        .onChange(of: isRefresh) { isRefresh in
+          if isRefresh == true {
+            withAnimation { // TODO: 애니메이션 여부 체크
+              proxy.scrollTo(leadingID, anchor: .topLeading)
+            }
+            self.isRefresh.toggle()
+          }
+        }
+      }
+    }
+  }
+  
+  struct BubbleChartView: View {
+    var hotKeyword: HotKeywordPointList
+    @Binding var offset: CGFloat {
+      didSet {
+        print(offset)
+      }
+    }
+    
+    var body: some View {
+      GeometryReader { geometry in
+        ForEach(hotKeyword.pointList, id: \.self) { point in
+          BubbleView(
+            keyword: point.keyword,
+            bubbleSize: point.size,
+            bubbleColor: point.color,
+            geometryHeight: geometry.size.height,
+            pointX: point.x * geometry.size.width,
+            offset: $offset
+          )
+          .offset(
+            x: point.x * geometry.size.width,
+            y: point.y * geometry.size.height
+          )
+        }
+      }
+    }
+  }
+  
+  // MARK: - 스크롤 관련
+  private var scrollObservableView: some View {
+    GeometryReader { geometry in
+      let offsetX = -geometry.frame(in: .global).origin.x
+      Color.clear
+        .preference(
+          key: ScrollOffsetKey.self,
+          value: offsetX
+        )
+    }
+    .frame(width: 0)
+  }
+  
+  struct ScrollOffsetKey: PreferenceKey {
+    static var defaultValue: CGFloat = .zero
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+      value += nextValue()
+    }
   }
 }
