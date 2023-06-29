f6138b2a64e9707f44c4ea00e60dc6d80516b45d
김영균 <48887389+klmyoungyun@users.noreply.github.com>
2023-06-29 18:55:57 +0900

    기능: 저장 후 다음 뉴스카드로 자동 스크롤 구현  (#92)
    * 기능: 저장 후 자동 스크롤 구현
    
    * 수정: 저장 성공시에만 넘어가도록 변경
    
    * 수정: 네이밍 변경
    
    - Url에서 URL로 변경
    - newsCardId에서 newsCardID로 변경

--- Projects/Core/Models/Network/NewsCard/ResponseDTO/NewsResponseDTO.swift
+++ Projects/Core/Models/Network/NewsCard/ResponseDTO/NewsResponseDTO.swift
@@ -11,9 +11,19 @@ import Foundation
 public struct NewsResponseDTO: Decodable {
   public let id: Int
   public let title: String
-  public let thumbnailImageUrl: String
+  public let thumbnailImageURL: String
   public let newsLink: String
   public let press: String
   public let writtenDateTime: String
   public let type: String
+  
+  enum CodingKeys: String, CodingKey {
+    case id
+    case title
+    case thumbnailImageURL = "thumbnailImageUrl"
+    case newsLink
+    case press
+    case writtenDateTime
+    case type
+  }
 }
