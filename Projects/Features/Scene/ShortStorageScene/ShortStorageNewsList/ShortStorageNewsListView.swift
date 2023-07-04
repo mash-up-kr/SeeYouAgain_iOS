014ae6cab4edd1f0bc56d05c10d8e677517fe342
lina <lina@weversecompany.com>
2023-07-05 00:02:02 +0900

    기능: 마이페이지 툴팁 구현

--- Projects/Features/Scene/ShortStorageScene/ShortStorageNewsList/ShortStorageNewsListView.swift
+++ Projects/Features/Scene/ShortStorageScene/ShortStorageNewsList/ShortStorageNewsListView.swift
@@ -68,20 +68,28 @@ public struct ShortStorageNewsListView: View {
                   
                   Text(viewStore.state.remainTimeString)
                     .font(.b16)
+                    .monospacedDigit()
                 }
                 .foregroundColor(DesignSystem.Colors.blue200)
                 
                 Button {
-                  // TODO: 안내 사항 표시
+                  viewStore.send(.tooltipButtonTapped)
                 } label: {
                   DesignSystem.Icons.iconSuggestedCircle
                     .frame(width: 16, height: 16)
                 }
               }
-              
-              Spacer()
-                .frame(height: 48)
-              
+
+              if viewStore.state.isDisplayTooltip {
+                TooltipView(store: store)
+                  .padding(.leading, 20)
+                  .padding(.bottom, -55) // VStack 내부에서 겹치게 하기 위해서 마이너스로 설정
+                  .zIndex(1) // 스크롤 할 때 같이 내려가야해서 VStack에 넣고, 맨 위에 겹치도록 zIndex 설정
+              } else {
+                Spacer()
+                  .frame(height: 48)
+              }
+             
               ForEachStore(
                 self.store.scope(
                   state: \.shortsNewsItems,
@@ -167,5 +175,91 @@ private struct EmptyNewsContentView: View {
         .foregroundColor(DesignSystem.Colors.grey70)
         .padding(.horizontal, 24)
     }
+  }
+}
+
+// MARK: - Tooltip View
+private struct TooltipView: View {
+  private let store: Store<ShortStorageNewsListState, ShortStorageNewsListAction>
+  
+  fileprivate init(store: Store<ShortStorageNewsListState, ShortStorageNewsListAction>) {
+    self.store = store
+  }
+  
+  fileprivate var body: some View {
+    WithViewStore(store) { viewStore in
+      // TriangleShape으로 사각형 가려야해서, 겹치도록 spcing 마이너스로 설정
+      VStack(spacing: -6) {
+        ZStack {
+          // 산 모양 라인
+          trianglePathView
+            .padding(.leading, 115)
+
+          // 아래 네모칸 윗부분 일정 부분 가리기 위한 삼각형
+          TriangleShape()
+            .frame(width: 20, height: 15)
+            .padding(.leading, 115)
+            .padding(.bottom, -2) // 산모양 라인이랑 겹치지 않게 하기 위해 내려와있음
+            .foregroundColor(.white)
+        }
+        .frame(width: 20, height: 15)
+        .zIndex(1)
+        
+        toolTipContentView
+      }
+    }
+  }
+  
+  private var trianglePathView: some View {
+    Path { path in
+      path.move(to: CGPoint(x: 0, y: 15))
+      path.addLine(to: CGPoint(x: 7.5, y: 2))
+      path.addQuadCurve(to: CGPoint(x: 12.5, y: 2), control: CGPoint(x: 10, y: 0))
+      path.addLine(to: CGPoint(x: 20, y: 15))
+    }
+    .stroke(DesignSystem.Colors.grey80, lineWidth: 1)
+  }
+  
+  private var toolTipContentView: some View {
+    WithViewStore(store) { viewStore in
+      ZStack {
+        Rectangle()
+          .foregroundColor(.white)
+          .frame(width: 282, height: 83)
+          .cornerRadius(8)
+          .overlay(
+            RoundedRectangle(cornerRadius: 8)
+              .stroke(DesignSystem.Colors.grey80, lineWidth: 1)
+          )
+        
+        HStack(alignment: .top, spacing: 8) {
+          Text("‘오늘의 숏스’는 자정이 지나면 사라져요. 오늘 안에 읽고, 오늘이 지나도 보고 싶은 숏스는 ‘오래 간직할 숏스'로 옮겨보세요.")
+            .font(.r14)
+            .foregroundColor(DesignSystem.Colors.grey80)
+          
+          Button {
+            viewStore.send(.tooltipButtonTapped)
+          } label: {
+            DesignSystem.Icons.iconX
+              .frame(width: 16, height: 16)
+          }
+        }
+        .frame(width: 246, alignment: .topLeading)
+      }
+      .frame(width: 282, height: 94)
+    }
+  }
+}
+
+private struct TriangleShape: Shape {
+  fileprivate func path(in rect: CGRect) -> Path {
+    var path = Path()
+    
+    path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+    path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+    path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+    path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+    
+    return path
   }
 }
