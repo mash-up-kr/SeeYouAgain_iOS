ffe73e428781a2ad6891f5edb97e90dfade43009
younggyun <kyoungyun98@naver.com>
2023-07-07 14:33:29 +0900

    수정: 편지지 이미지 대체로 인한 Shape 제거

--- Projects/Features/Scene/MainScene/Main/NewsCard/Components/LetterTop.swift
+++ Projects/Features/Scene/MainScene/Main/NewsCard/Components/LetterTop.swift
@@ -6,82 +6,28 @@
 //  Copyright © 2023 mashup.seeYouAgain. All rights reserved.
 //
 
+import ComposableArchitecture
 import DesignSystem
 import SwiftUI
 
-private struct LetterTopTriangle: Shape {
-  private let deviceRatio: CGSize
-  
-  fileprivate init(deviceRatio: CGSize) {
-    self.deviceRatio = deviceRatio
-  }
-  
-  fileprivate func path(in rect: CGRect) -> Path {
-    let topSpacing: CGFloat = 30 * deviceRatio.height
-    let cap: CGFloat = 76.5 * deviceRatio.height
-    var path = Path()
-    path.move(to: CGPoint(x: rect.minX, y: rect.minY))
-    path.addArc(
-      tangent1End: CGPoint(x: rect.minX, y: rect.minY + cap),
-      tangent2End: CGPoint(x: rect.midX, y: rect.maxY),
-      radius: 6
-    )
-    path.addArc(
-      tangent1End: CGPoint(x: rect.midX, y: rect.maxY - topSpacing),
-      tangent2End: CGPoint(x: rect.maxX, y: rect.minY + cap),
-      radius: 30
-    )
-    path.addArc(
-      tangent1End: CGPoint(x: rect.maxX, y: rect.minY + cap),
-      tangent2End: CGPoint(x: rect.maxX, y: rect.minY),
-      radius: 6
-    )
-    path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-    path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
-    return path
-  }
-}
-
 struct LetterTop: View {
-  @Binding private var isFold: Bool
-  private let deviceRatio: CGSize
+  private let store: Store<Bool, Never>
   
-  init(isFold: Binding<Bool>, deviceRatio: CGSize) {
-    self._isFold = isFold
-    self.deviceRatio = deviceRatio
+  init(store: Store<Bool, Never>) {
+    self.store = store
   }
   
   var body: some View {
-    GeometryReader { geometry in
+    WithViewStore(store) { viewStore in
       VStack {
         Spacer()
         
-        OpaqueShape(
-          color: LinearGradient(
-            colors: [
-              DesignSystem.Colors.white,
-              DesignSystem.Colors.white.opacity(0.5)
-          ],
-            startPoint: .top,
-            endPoint: .bottom
-          ),
-          strokeColor: LinearGradient(
-            colors: [
-              DesignSystem.Colors.white,
-              DesignSystem.Colors.white.opacity(0)
-          ],
-            startPoint: .top,
-            endPoint: .bottom
-          ),
-          strokeOpacity: 0.6,
-          shape: { LetterTopTriangle(deviceRatio: deviceRatio) }
-        )
-        .frame(height: geometry.size.height / 2)
-        .cornerRadius(isFold ? 20 : 0, corners: [.topLeft, .topRight])
-        .opacity(isFold ? 1 : 0)
-        .animation(.easeInOut, value: isFold)
+        DesignSystem.Images.letterTop
+          .resizable()
+          .scaledToFit()
+          .opacity(viewStore.state ? 1 : 0)
       }
-      .rotation3DEffect(.degrees(isFold ? 0 : 180), axis: (x: 1, y: 0, z: 0))
+      .rotation3DEffect(.degrees(viewStore.state ? 0 : 180), axis: (x: 1, y: 0, z: 0))
     }
   }
 }
