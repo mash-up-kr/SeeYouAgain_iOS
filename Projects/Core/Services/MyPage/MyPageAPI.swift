4f57fc6bf54da94bb2bfffc6f523fa5ca899aed4
Chanwoo Cho <72292617+GREENOVER@users.noreply.github.com>
2023-07-09 11:13:49 +0900

    수정: 서버 URL 변경 (#150)

--- Projects/Core/Services/MyPage/MyPageAPI.swift
+++ Projects/Core/Services/MyPage/MyPageAPI.swift
@@ -20,7 +20,7 @@ public enum MyPageAPI {
 
 extension MyPageAPI: TargetType {
   public var baseURL: URL {
-    return URL(string: "http://3.38.65.72:8080/v1")!
+    return URL(string: "http://3.36.227.253:8080/v1")!
   }
   
   public var path: String {
