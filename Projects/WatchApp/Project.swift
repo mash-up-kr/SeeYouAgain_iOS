62c89c0d2a614abd8966c5262d099097af43911e
younggyun <kyoungyun98@naver.com>
2023-07-30 05:04:27 +0900

    기능: 애플 워치 타겟 추가

--- Projects/WatchApp/Project.swift
+++ Projects/WatchApp/Project.swift
@@ -2,56 +2,58 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.make(
-  name: "WatchApp",
-  targets: [
-    .make(
-      name: "Prod-SeeYouAgain-WatchOS",
-      platform: .watchOS,
-      product: .watch2Extension,
-      bundleId: "com.mashup.seeYouAgain.watchkitapp",
-      deploymentTarget: .watchOS(targetVersion: "9.0"),
-      infoPlist: .file(path: .relativeToRoot("Projects/WatchApp/Info.plist")),
-      sources: ["Sources/**"],
-      resources: ["Resources/**"],
-      dependencies: [
-        .project(target: "CoreKitWatchOS", path: .relativeToRoot("Projects/Core")),
-        .project(target: "DesignSystemWatchOS", path: .relativeToRoot("Projects/DesignSystemWatchOS")),
-        .externalsrt("TCA"),
-      ],
-      settings: .settings(
-        base: [
-          "ASSETCATALOG_COMPILER_APPICON_NAME":"ProdAppIcon"
-        ],
-        configurations: [
-          .release(name: .release, xcconfig: "./xcconfigs/SeeYouAgain.release.xcconfig"),
-        ]
-      )
-    ),
-    .make(
-      name: "Dev-SeeYouAgain-WatchOS",
-      platform: .watchOS,
-      product: .watch2Extension,
-      bundleId: "com.mashup.seeYouAgain.watchkitapp-dev",
-      deploymentTarget: .watchOS(targetVersion: "9.0"),
-      infoPlist: .file(path: .relativeToRoot("Projects/WatchApp/Info.plist")),
-      sources: ["Sources/**"],
-      resources: ["Resources/**"],
-      dependencies: [
-        .project(target: "CoreKitWatchOS", path: .relativeToRoot("Projects/Core")),
-        .project(target: "DesignSystemWatchOS", path: .relativeToRoot("Projects/DesignSystemWatchOS")),
-        .externalsrt("TCA"),
-      ],
-      settings: .settings(
-        base: [
-          "ASSETCATALOG_COMPILER_APPICON_NAME":"DevAppIcon"
-        ],
-        configurations: [
-          .debug(name: .debug, xcconfig: "./xcconfigs/SeeYouAgain.debug.xcconfig"),
-        ]
-      )
-    )
-  ],
-  additionalFiles: [
-    "./xcconfigs/SeeYouAgain.shared.xcconfig"
-  ]
+	name: "WatchApp",
+	targets: [
+		.make(
+			name: "Prod-SeeYouAgain-WatchOS",
+			platform: .watchOS,
+			product: .app,
+			bundleId: "com.mashup.seeYouAgain.watchkitapp",
+			deploymentTarget: .watchOS(targetVersion: "9.0"),
+			infoPlist: .file(path: .relativeToRoot("Projects/WatchApp/Info.plist")),
+			sources: ["Sources/**"],
+			resources: ["Resources/**"],
+			dependencies: [
+				.project(target: "CoreKitWatchOS", path: .relativeToRoot("Projects/Core")),
+				.project(target: "DesignSystemWatchOS", path: .relativeToRoot("Projects/DesignSystemWatchOS")),
+				.externalsrt("TCA"),
+			],
+			settings: .settings(
+				base: [
+					"INFOPLIST_KEY_WKRunsIndependentlyOfCompanionApp": false,
+					"ASSETCATALOG_COMPILER_APPICON_NAME":"ProdAppIcon"
+				],
+				configurations: [
+					.release(name: .release, xcconfig: "./xcconfigs/SeeYouAgain.release.xcconfig"),
+				]
+			)
+		),
+		.make(
+			name: "Dev-SeeYouAgain-WatchOS",
+			platform: .watchOS,
+			product: .app,
+			bundleId: "com.mashup.seeYouAgain.watchkitapp-dev",
+			deploymentTarget: .watchOS(targetVersion: "9.0"),
+			infoPlist: .file(path: .relativeToRoot("Projects/WatchApp/Info.plist")),
+			sources: ["Sources/**"],
+			resources: ["Resources/**"],
+			dependencies: [
+				.project(target: "CoreKitWatchOS", path: .relativeToRoot("Projects/Core")),
+				.project(target: "DesignSystemWatchOS", path: .relativeToRoot("Projects/DesignSystemWatchOS")),
+				.externalsrt("TCA"),
+			],
+			settings: .settings(
+				base: [
+					"INFOPLIST_KEY_WKRunsIndependentlyOfCompanionApp": false,
+					"ASSETCATALOG_COMPILER_APPICON_NAME":"DevAppIcon"
+				],
+				configurations: [
+					.debug(name: .debug, xcconfig: "./xcconfigs/SeeYouAgain.debug.xcconfig"),
+				]
+			)
+		)
+	],
+	additionalFiles: [
+		"./xcconfigs/SeeYouAgain.shared.xcconfig"
+	]
 )
