b1cc21adad8ec87cdd451fc74d3e1e3ac9b2df6a
chanwoo <humains29@gmail.com>
2023-07-30 04:06:46 +0900

    인프라 테스트

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -48,6 +48,52 @@ let project = Project.make(
         ]
       )
     ),
+    .make(
+      name: "Prod-SeeYouAgain-WatchOS",
+      platform: .watchOS,
+      product: .watch2Extension,
+      bundleId: "com.mashup.seeYouAgain.watchkitextension",
+      deploymentTarget: .watchOS(targetVersion: "9.0"),
+      infoPlist: .file(path: .relativeToRoot("Projects/App/WatchInfo.plist")),
+      sources: ["Sources/**"],
+      resources: ["Resources/**"],
+      dependencies: [
+        .project(target: "CoreKitWatchOS", path: .relativeToRoot("Projects/Core")),
+        .project(target: "DesignSystemWatchOS", path: .relativeToRoot("Projects/DesignSystemWatchOS")),
+        .externalsrt("TCA"),
+      ],
+      settings: .settings(
+        base: [
+          "ASSETCATALOG_COMPILER_APPICON_NAME":"ProdAppIcon"
+        ],
+        configurations: [
+          .release(name: .release, xcconfig: "./xcconfigs/SeeYouAgain.release.xcconfig"),
+        ]
+      )
+    ),
+    .make(
+      name: "Dev-SeeYouAgain-WatchOS",
+      platform: .watchOS,
+      product: .watch2Extension,
+      bundleId: "com.mashup.seeYouAgain.watchkitapp-dev",
+      deploymentTarget: .watchOS(targetVersion: "9.0"),
+      infoPlist: .file(path: .relativeToRoot("Projects/App/WatchInfo.plist")),
+      sources: ["Sources/**"],
+      resources: ["Resources/**"],
+      dependencies: [
+        .project(target: "CoreKitWatchOS", path: .relativeToRoot("Projects/Core")),
+        .project(target: "DesignSystemWatchOS", path: .relativeToRoot("Projects/DesignSystemWatchOS")),
+        .externalsrt("TCA"),
+      ],
+      settings: .settings(
+        base: [
+          "ASSETCATALOG_COMPILER_APPICON_NAME":"DevAppIcon"
+        ],
+        configurations: [
+          .debug(name: .debug, xcconfig: "./xcconfigs/SeeYouAgain.debug.xcconfig"),
+        ]
+      )
+    )
   ],
   additionalFiles: [
     "./xcconfigs/SeeYouAgain.shared.xcconfig"
