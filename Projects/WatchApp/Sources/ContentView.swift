2edb3a9374ff5c92a761645aa3125393351e5746
lina <lina@weversecompany.com>
2023-07-30 05:09:40 +0900

    Revert "인프라 테스트"
    This reverts commit b1cc21adad8ec87cdd451fc74d3e1e3ac9b2df6a.

---
+++ Projects/WatchApp/Sources/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  WatchApp
+//
+//  Created by 안상희 on 2023/07/29.
+//  Copyright © 2023 mashup.seeYouAgain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello World")
+  }
+}
