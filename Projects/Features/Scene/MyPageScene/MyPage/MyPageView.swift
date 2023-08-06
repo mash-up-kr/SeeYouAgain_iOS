413f6a260eb169c60d939d1e9a1f0edd04f417c5
Sanghee <dev.sanghee@gmail.com>
2023-08-06 12:21:54 +0900

    디자인: 마이페이지 블러 배경 추가

--- Projects/Features/Scene/MyPageScene/MyPage/MyPageView.swift
+++ Projects/Features/Scene/MyPageScene/MyPage/MyPageView.swift
@@ -22,51 +22,59 @@ public struct MyPageView: View {
       ZStack {
         ScrollView {
           VStack(spacing: 0) {
-            TopNavigationBar(
-              rightIcon: DesignSystem.Icons.iconSetting,
-              rightIconButtonAction: {
-                viewStore.send(.settingButtonTapped)
-              }
-            )
-            
-            ZStack(alignment: .topTrailing) {
-              DesignSystem.Images.imageEarth
-                .frame(width: 124, height: 121)
-                .padding(.trailing, 24)
-              
-              MyInfoView(
-                store: store.scope(
-                  state: \.info,
-                  action: MyPageAction.info
-                )
+            VStack(spacing: 0) {
+              TopNavigationBar(
+                rightIcon: DesignSystem.Icons.iconSetting,
+                rightIconButtonAction: {
+                  viewStore.send(.settingButtonTapped)
+                }
               )
-              .padding(.horizontal, 24)
+              
+              ZStack(alignment: .topTrailing) {
+                DesignSystem.Images.imageEarth
+                  .frame(width: 124, height: 121)
+                  .padding(.trailing, 24)
+                
+                MyInfoView(
+                  store: store.scope(
+                    state: \.info,
+                    action: MyPageAction.info
+                  )
+                )
+                .padding(.horizontal, 24)
+                
+                Spacer()
+              }
+              .padding(.top, 32)
               
               Spacer()
+                .frame(height: 40)
             }
-            .padding(.top, 32)
+            .myPageBackgroundView()
             
-            Spacer()
-              .frame(height: 20)
-            
-            StatisticsView(
-              store: store.scope(
-                state: \.statistics,
-                action: MyPageAction.statisticsAction
+            VStack(spacing: 0) {
+              Spacer()
+                .frame(height: 20)
+              
+              StatisticsView(
+                store: store.scope(
+                  state: \.statistics,
+                  action: MyPageAction.statisticsAction
+                )
               )
-            )
-            .padding(.horizontal, 24)
-            
-            Spacer()
-              .frame(height: 32)
-            
-            MyAchievementsView(
-              store: store.scope(
-                state: \.myAchievements,
-                action: MyPageAction.myAchievementsAction
+              
+              Spacer()
+                .frame(height: 32)
+              
+              MyAchievementsView(
+                store: store.scope(
+                  state: \.myAchievements,
+                  action: MyPageAction.myAchievementsAction
+                )
               )
-            )
+            }
             .padding(.horizontal, 24)
+            .myStatisticsBackgroundView()
           }
           .onAppear {
             viewStore.send(._onAppear)
@@ -74,7 +82,6 @@ public struct MyPageView: View {
         }
         .navigationBarHidden(true)
         .edgesIgnoringSafeArea(.bottom)
-        .myPageBackgroundView()
       }
     }
   }
@@ -102,10 +109,55 @@ fileprivate struct MyPageBackgroundView: View {
   }
 }
 
+fileprivate struct MyStatisticsBackgroundView: View {
+  fileprivate init() {}
+  
+  fileprivate var body: some View {
+    ZStack {
+      DesignSystem.Colors.coolgrey100
+        .opacity(1)
+        .edgesIgnoringSafeArea(.all)
+      
+      GeometryReader { geometry in
+        Circle()
+          .fill(
+            Color(red: 159/255, green: 114/255, blue: 255/255, opacity: 0.5)
+          )
+          .frame(width: 153, height: 153)
+          .blur(radius: 96)
+          .offset(x: 256, y: 23)
+        
+        Circle()
+          .fill(
+            Color(red: 114/255, green: 187/255, blue: 255/255, opacity: 0.3)
+          )
+          .frame(width: 189, height: 189)
+          .blur(radius: 96)
+          .offset(x: -44, y: 299)
+        
+        Circle()
+          .fill(
+            Color(red: 130/255, green: 255/255, blue: 247/255, opacity: 0.4)
+          )
+          .frame(width: 113, height: 113)
+          .blur(radius: 96)
+          .offset(x: 307, y: 510)
+      }
+    }
+  }
+}
+
 extension View {
   fileprivate func myPageBackgroundView() -> some View {
     ZStack {
       MyPageBackgroundView()
+      self
+    }
+  }
+  
+  fileprivate func myStatisticsBackgroundView() -> some View {
+    ZStack {
+      MyStatisticsBackgroundView()
       self
     }
   }
